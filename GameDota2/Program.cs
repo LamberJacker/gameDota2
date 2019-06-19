4eba947931c07579ad6db6c463e4c9bbbbde06f1
Vladyslav Martynovskyi <50924124+LamberJacker@users.noreply.github.com>
2019-06-19 11:33:02 +0300

    some trouble

--- GameDota2/Program.cs
+++ GameDota2/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace GameLegueOfLegend
 {
@@ -16,27 +16,45 @@ namespace GameLegueOfLegend
             Console.WriteLine("Кто победит? На это велит воля случая и конечно же владения своими навыками...");
             Console.WriteLine("Да начнеться битва...");
             Console.ResetColor();
-            Boolean flag = false;
+            Boolean flag = true;
             Hero firstHero = new Hero();
             Hero secondHero = new Hero();
+            int turn = 0;
             do
             {
-                
-                if (firstHero.getHP <= 0D)
-                {
-                    Console.WriteLine("Проиграл " + firstHero.getName);
-                    flag = false;
-                }
-                else if (secondHero.getHP <= 0D)
-                {
-                    Console.WriteLine("Проиграл " + secondHero.getName);
-                    flag = false;
-                }
-                else
-                {
-                    Console.WriteLine("Герой №1 " + firstHero.getInfo);
-                }
-
+              Console.WriteLine("Герой #1 " + firstHero.getInfo());
+              Console.WriteLine("Герой #2 " + secondHero.getInfo());
+              System.Threading.Thread.Sleep(1000);  
+              if (firstHero.getHP() <= 0D)
+              {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вот он лузер! {0} умирает!", firstHero.getName());
+                Console.ResetColor();
+                flag = false;
+              }
+              else if (secondHero.getHP() <= 0D)
+              {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вот он лузер! {0} умирает!", secondHero.getName());
+                Console.ResetColor();
+                flag = false;
+              }
+              else if (turn%2 == 0)
+              {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Бьет герой #1 : {0}, и наносит {1} урона!", firstHero.getName(), firstHero.getStrikepower());
+                secondHero.setHP(secondHero.getHP()-firstHero.getStrikepower());
+                Console.ResetColor();
+                turn++;
+              }
+              else
+              {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Бьет герой #2 : {0}, и наносит {1} урона!", secondHero.getName(), secondHero.getStrikepower());
+                firstHero.setHP(firstHero.getHP()-secondHero.getStrikepower());
+                Console.ResetColor();
+                turn++;
+              }
             } while (flag == true);
         }
         //private void getInfoHero()
@@ -58,17 +76,17 @@ namespace GameLegueOfLegend
         public Hero()
         {
             Random rnd = new Random();
-            int rndHero = rnd.Next(0, 3);
+            int rndHero = rnd.Next(1, 3);
             switch (rndHero)
             {
                 case 1:
-                    Teemo teemo = new Teemo();
+                    new Teemo();
                     break;
                 case 2:
-                    Draven draven = new Draven();
+                    new Draven();
                     break;
                 case 3:
-                    Brand brand = new Brand();
+                    new Brand();
                     break;
             }
         }
@@ -80,7 +98,7 @@ namespace GameLegueOfLegend
         {
             return health;
         }
-        public double setHP(double health)
+        public void setHP(double health)
         {
             this.health = health;
         }
@@ -95,38 +113,38 @@ namespace GameLegueOfLegend
     }
     class Teemo : Hero
     {
-        private void setCharacteristic()
+        public Teemo()
         {
-            this.name = "Teemo";
-            this.health = 150D;
-            this.intellect = 5D;
-            this.agility = 7D;
-            this.strength = 4D;
-            this.strikepower = (1D * intellect + 1.4 * agility + (strength/health+1) * strength);
+            base.name = "Teemo";
+            base.health = 150D;
+            base.intellect = 5D;
+            base.agility = 7D;
+            base.strength = 4D;
+            base.strikepower = (1D * intellect + 1.4 * agility + (strength/health+1) * strength);
         }
     }
     class Draven : Hero
     {
-        private void setCharacteristic()
+        public Draven()
         {
-            this.name = "Draven";
-            this.health = 160D;
-            this.intellect = 3D;
-            this.agility = 6D;
-            this.strength = 8D;
-            this.strikepower = (0.9 * intellect + 1.6 * agility + (strength / health * 1.05) * strength);
+            base.name = "Draven";
+            base.health = 160D;
+            base.intellect = 3D;
+            base.agility = 6D;
+            base.strength = 8D;
+            base.strikepower = (0.9 * intellect + 1.6 * agility + (strength / health * 1.05) * strength);
         }
     }
-    class Brand : Hero
+    class Brand : Hero 
     {
-        private void setCharacteristic()
+        public Brand()
         {
-            this.name = "Brand";
-            this.health = 120D;
-            this.intellect = 9D;
-            this.agility = 3D;
-            this.strength = 1D;
-            this.strikepower = (2D * intellect + 0.97 * agility + 1D * strength);
+            base.name = "Brand";
+            base.health = 120D;
+            base.intellect = 9D;
+            base.agility = 3D;
+            base.strength = 1D;
+            base.strikepower = (2D * intellect + 0.97 * agility + 1D * strength);
         }
     }
     //class Item
@@ -140,14 +158,14 @@ namespace GameLegueOfLegend
     {
         static void Main()
         {
-            Console.WriteLine("Что бы начать игру впиши \"KILL THEM ALL!\", для выхода нажми \"q\"");
             Boolean check = true;
             do
             {
+                Console.WriteLine("Что бы начать игру напиши \"s\", для выхода нажми \"q\"");
                 string inputFromUser = Console.ReadLine();
                 switch (inputFromUser)
                 {
-                    case "KILL THEM ALL!":
+                    case "s":
                         new Game();
                         break;
                     case "q":
